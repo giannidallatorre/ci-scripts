e1bd8fbe186e52c1ad02f473fefa81f99a6e2ebc
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-10-22 11:37:28 +0200

    Translated volumes relative paths to absolute

--- docker/setup_deployment.sh
+++ docker/setup_deployment.sh
@@ -7,7 +7,7 @@ PLATFORM="${PLATFORM:-SL6}"
 git clone https://github.com/dandreotti/docker-scripts.git
 cd storm-deployment-test
 
-mkdir -p ./docker_storm/storage
-mkdir -p ./docker_storm/logs
+mkdir -p $PWD/docker_storm/storage
+mkdir -p $PWD/docker_storm/logs
 
-docker run -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v ./docker_storm/storage:/storage:rw -v ./docker_storm/logs:/var/log/storm:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0
+docker run -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v $PWD/docker_storm/storage:/storage:rw -v $PWD/docker_storm/logs:/var/log/storm:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0
