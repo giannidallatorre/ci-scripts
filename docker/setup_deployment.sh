93b466c3fa6d5e9b1ac6077784f273f84844a3cf
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-10-23 14:04:28 +0200

    Fixed typo

--- docker/setup_deployment.sh
+++ docker/setup_deployment.sh
@@ -7,14 +7,14 @@ PLATFORM="${PLATFORM:-SL6}"
 mkdir -p $PWD/docker_storm/storage
 
 # run StoRM deployment
-docker run -d -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v $PWD/docker_storm/storage:/storage:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0 docker run -d -e "MODE=clean" -e "PLATFORM=SL6" -h docker-storm.cnaf.infn.it -t -i  -v ~/docker_storm/storage:/storage:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0 /bin/sh deploy.sh
+docker run -d -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v $PWD/docker_storm/storage:/storage:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0 
 
 # run StoRM testsuite when deployment is over
-docker run --link storm-deploy:docker-storm.cnaf.infn.it --volumes-from storm-deploy -v /etc/localtime:/etc/localtime:ro --name storm-ts centos6/storm-ts:1.0
+docker run --link storm-deploy:docker-storm.cnaf.infn.it --volumes-from storm-deploy -v /etc/localtime:/etc/localtime:ro --name storm-ts-linked centos6/storm-ts:1.0
 
 # copy StoRM logs 
 docker cp storm-deploy:/var/log/storm .
 
 # remove containers
 docker rm -f storm-deploy
-docker rm -f storm-ts
+docker rm -f storm-ts-linked
