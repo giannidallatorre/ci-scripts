401830b3c0745626c827bb3494d3e4930be489e8
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-10-23 14:24:11 +0200

    Added start script for the deployment container. Got deployment log.

--- docker/setup_deployment.sh
+++ docker/setup_deployment.sh
@@ -7,13 +7,16 @@ PLATFORM="${PLATFORM:-SL6}"
 mkdir -p $PWD/docker_storm/storage
 
 # run StoRM deployment
-docker run -d -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v $PWD/docker_storm/storage:/storage:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0 
+docker run -d -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v $PWD/docker_storm/storage:/storage:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0 /bin/sh deploy.sh
 
 # run StoRM testsuite when deployment is over
 docker run --link storm-deploy:docker-storm.cnaf.infn.it --volumes-from storm-deploy -v /etc/localtime:/etc/localtime:ro --name storm-ts-linked centos6/storm-ts:1.0
 
 # copy StoRM logs 
 docker cp storm-deploy:/var/log/storm .
+
+# get deployment log
+docker logs --tail="all" storm-deploy &> storm-deployment.log
 
 # remove containers
 docker rm -f storm-deploy
