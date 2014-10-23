51d44c2ce0bf6ec2c095148396e25bb61223b28f
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-10-23 14:30:29 +0200

    Waiting few seconds before starting the testsuite

--- docker/setup_deployment.sh
+++ docker/setup_deployment.sh
@@ -9,6 +9,9 @@ mkdir -p $PWD/docker_storm/storage
 # run StoRM deployment
 docker run -d -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" -h docker-storm.cnaf.infn.it -v $PWD/docker_storm/storage:/storage:rw -v /etc/localtime:/etc/localtime:ro --name storm-deploy centos6/storm-deploy:1.0 /bin/sh deploy.sh
 
+# wait
+sleep 3
+
 # run StoRM testsuite when deployment is over
 docker run --link storm-deploy:docker-storm.cnaf.infn.it --volumes-from storm-deploy -v /etc/localtime:/etc/localtime:ro --name storm-ts-linked centos6/storm-ts:1.0
 
