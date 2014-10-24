a94498baf6f33f5d798d06f34be1c6f5afbf98d1
giannidallatorre <giannidallatorre@gmail.com>
2014-10-24 19:06:27 +0200

    Update deploy&test_voms.sh

--- docker/deploy&test_voms.sh
+++ docker/deploy&test_voms.sh
@@ -3,27 +3,38 @@ set -x
 
 MODE="${MODE:-clean}"
 PLATFORM="${PLATFORM:-SL6}"
+VO="${VO:-test.vo}"
+VO_HOST="${VO_HOST:-vgrid02.cnaf.infn.it}"
+VO_ISSUER="${VO_ISSUER:-/C=IT/O=INFN/OU=Host/L=CNAF/CN=vgrid02.cnaf.infn.it}"
+VOMSREPO="${VOMSREPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_voms_develop_SL6/lastSuccessfulBuild/artifact/voms-develop_sl6.repo}"
+
 
 mkdir -p /tmp/docker_voms/storage
 
 # run VOMS deployment
-docker run -d -P -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" \
+docker run -d -P -e "MODE=${MODE}" \
+  -e "PLATFORM=${PLATFORM}" \
   -h voms-server \
-  -v /tmp/docker_voms/storage:/storage:rw \
   -v /etc/localtime:/etc/localtime:ro \
+  -v /tmp/docker_voms/storage:/storage:rw \
   --name voms-server \
-  centos6/voms-server:1.0 \
-  /bin/sh deploy.sh
+  centos6/voms-server:1.0
 
 # run VOMS testsuite when deployment is over
-docker run --link voms-server:voms-server \
-  --volumes-from voms-server \
+docker run -e "VO=${VO}" \
+  -e "VO_HOST=${VO_HOST}" \
+  -e "VO_ISSUER=${VO_ISSUER}" \
+  -e "VOMSREPO=${VOMSREPO}" \  
+  -h voms-ts \
   -v /etc/localtime:/etc/localtime:ro \
-  --name voms-ts-linked \
+  --name voms-ts \
+  --link voms-server:voms-server \
+  --volumes-from voms-server \
   centos6/voms-ts:1.0
 
-# copy testsuite reports 
-docker cp voms-ts-linked:/home/tester/voms-testsuite/reports .
+# copy testsuite reports
+mkdir voms-ts_reports
+docker cp voms-ts:/home/voms/voms-testsuite/reports voms-ts_reports/
 
 # copy VOMS logs 
 docker cp voms-server:/var/log/voms .
