aba192553c1529ac5a5275ada9bac1328d5f87f8
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-11-03 12:11:18 +0100

    Changes for parallel containers execution by using Docker naming

--- docker/setup_deployment.sh
+++ docker/setup_deployment.sh
@@ -7,30 +7,34 @@ STORM_REPO="${STORM_REPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo
 
 mkdir -p /tmp/docker_storm/storage
 
-# run StoRM deployment
-docker run -d -e "STORM_REPO=${STORM_REPO}" -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" \
+# run StoRM deployment and get its id
+deploy_id=`docker run -d -e "STORM_REPO=${STORM_REPO}" -e "MODE=${MODE}" -e "PLATFORM=${PLATFORM}" \
   -h docker-storm.cnaf.infn.it \
   -v /tmp/docker_storm/storage:/storage:rw \
   -v /etc/localtime:/etc/localtime:ro \
-  --name storm-deploy \
   centos6/storm-deploy:1.0 \
-  /bin/sh deploy.sh
+  /bin/sh deploy.sh`
+
+# get names for deployment and testsuite containers
+deployment_name=`docker inspect -f "{{ .Name }}" $deploy_id|cut -c2-`
+testsuite_name="ts-linked-to-$deployment_name"
 
 # run StoRM testsuite when deployment is over
-docker run --link storm-deploy:docker-storm.cnaf.infn.it \
+docker run --link $deployment_name:docker-storm.cnaf.infn.it \
   -v /etc/localtime:/etc/localtime:ro \
-  --name storm-ts-linked \
+  --name $testsuite_name \
   centos6/storm-ts:1.0
 
 # copy testsuite reports 
-docker cp storm-ts-linked:/home/tester/storm-testsuite/reports .
+docker cp $testsuite_name:/home/tester/storm-testsuite/reports .
 
 # copy StoRM logs 
-docker cp storm-deploy:/var/log/storm .
+docker cp $deployment_name:/var/log/storm .
 
 # get deployment log
-docker logs --tail="all" storm-deploy &> storm-deployment.log
+docker logs --tail="all" $deployment_name &> storm-deployment.log
 
 # remove containers
-docker rm -f storm-deploy
-docker rm -f storm-ts-linked
+docker rm -f $deployment_name
+docker rm -f $testsuite_name
+
