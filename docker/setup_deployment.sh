a29be786eddf8764ef72c11cc7944a7a5b99f989
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-12-01 14:34:09 +0100

    Rollback

--- docker/setup_deployment.sh
+++ docker/setup_deployment.sh
@@ -21,7 +21,7 @@ deployment_name=`docker inspect -f "{{ .Name }}" $deploy_id|cut -c2-`
 testsuite_name="ts-linked-to-$deployment_name"
 
 # run StoRM testsuite when deployment is over
-docker run --rm=true --link $deployment_name:docker-storm.cnaf.infn.it \
+docker run --link $deployment_name:docker-storm.cnaf.infn.it \
   -v /etc/localtime:/etc/localtime:ro \
   --name $testsuite_name \
   italiangrid:storm-testsuite
@@ -37,4 +37,4 @@ docker logs --tail="all" $deployment_name &> storm-deployment.log
 
 # remove containers
 docker rm -f $deployment_name
-
+docker rm -f $testsuite_name
